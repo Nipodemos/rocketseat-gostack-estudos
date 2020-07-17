d40e575f47741f5f35d16d32f5f0d6ff4e1aacf2
Nipodemos <nipodemos13@gmail.com>
2020-07-17 00:45:52 -0300

    Upload de imagens

--- nivel02/banco_de_dados/src/routes/users.routes.ts
+++ nivel02/banco_de_dados/src/routes/users.routes.ts
@@ -1,7 +1,12 @@
 import { Router, Response, Request } from 'express';
+import multer from 'multer';
 import CreateUserService from '../services/CreateUserService';
+import ensureAthenticated from '../middlewares/ensureAuthenticated';
+import uploadConfig from '../config/uploads';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request: Request, response: Response) => {
   try {
@@ -21,5 +26,25 @@ usersRouter.post('/', async (request: Request, response: Response) => {
     return response.status(400).send({ error: err.message });
   }
 });
+
+usersRouter.patch(
+  '/avatar',
+  ensureAthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService();
+      const user = await updateUserAvatar.execute({
+        user_id: request.user.id,
+        avatarFilename: request.file.filename,
+      });
+      delete user.password;
+      console.log('request.file :>> ', request.file);
+      return response.json(user);
+    } catch (err) {
+      return response.status(400).send({ error: err.message });
+    }
+  },
+);
 
 export default usersRouter;
