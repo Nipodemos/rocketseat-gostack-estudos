1453d5e380f780481fc6bcb6460d25bbab323abb
Nipodemos <nipodemos13@gmail.com>
2020-08-13 06:30:49 -0300

    Fase 03 - Criando Aplicação: Estilizando Dashboard final

--- nivel03/primeiro-projeto-react/src/pages/Dashboard/index.tsx
+++ nivel03/primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 import logoImage from '../../assets/logo.svg';
-import { Title, Form } from './styles';
+import { Title, Form, Repositories } from './styles';
 
 const DashBoard: React.FC = () => {
   return (
@@ -10,8 +11,43 @@ const DashBoard: React.FC = () => {
 
       <Form>
         <input type="text" placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
       </Form>
-      <button type="submit">Pesquisar</button>
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/11494727?s=460&u=11f464c9901a87f930435eeb54c94ca09f211e96&v=4"
+            alt="Alan"
+          />
+          <div>
+            <strong>eventMacros</strong>
+            <p>All the macros made with eventMacro by me.</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/11494727?s=460&u=11f464c9901a87f930435eeb54c94ca09f211e96&v=4"
+            alt="Alan"
+          />
+          <div>
+            <strong>eventMacros</strong>
+            <p>All the macros made with eventMacro by me.</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/11494727?s=460&u=11f464c9901a87f930435eeb54c94ca09f211e96&v=4"
+            alt="Alan"
+          />
+          <div>
+            <strong>eventMacros</strong>
+            <p>All the macros made with eventMacro by me.</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
     </>
   );
 };
--- nivel03/primeiro-projeto-react/src/pages/Dashboard/styles.ts
+++ nivel03/primeiro-projeto-react/src/pages/Dashboard/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { shade } from 'polished';
 
 export const Title = styled.h1`
   font-size: 48px;
@@ -19,6 +20,10 @@ export const Form = styled.form`
     border: 0;
     padding: 0 24px;
     border-radius: 5px 0 0 5px;
+    color: #3a3a3a;
+    &::placeholder {
+      color: #a8a8b3;
+    }
   }
 
   button {
@@ -29,9 +34,55 @@ export const Form = styled.form`
     border: 0;
     color: #fff;
     font-weight: bold;
+    transition: background-color 0.3s;
 
     &:hover {
-      
+      background: ${shade(0.2, '#04d361')};
+    }
+  }
+`;
+
+export const Repositories = styled.div`
+  margin-top: 80px;
+  max-width: 700px;
+
+  a {
+    background-color: #fff;
+    border-radius: 5px;
+    width: 100%;
+    padding: 24px;
+    text-decoration: none;
+    display: flex;
+    align-items: center;
+    transition: transform 0.3s;
+    &:hover {
+      transform: translateX(10px);
+    }
+    img {
+      width: 64px;
+      height: 64px;
+      border-radius: 50%;
+    }
+
+    & + a {
+      margin-top: 16px;
+    }
+
+    div {
+      margin-left: 16px;
+
+      strong {
+        font-size: 20px;
+        color: #3d3d4d;
+      }
+      p {
+        font-size: 18px;
+        color: #a8a8b3;
+      }
+    }
+    svg {
+      margin-left: auto;
+      color: #cbcbd6;
     }
   }
 `;
