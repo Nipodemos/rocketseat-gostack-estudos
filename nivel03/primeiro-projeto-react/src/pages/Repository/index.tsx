77973a4121c6b827f5be77dc93cbdf42c52d7a37
Nipodemos <nipodemos13@gmail.com>
2020-09-08 19:02:14 -0300

    Fase 03 - Criando a aplicação: Navegando entre rotas

--- nivel03/primeiro-projeto-react/src/pages/Repository/index.tsx
+++ nivel03/primeiro-projeto-react/src/pages/Repository/index.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { useRouteMatch } from 'react-router-dom';
+
+interface RepositoryParams {
+  repository: string;
+}
 
 const Repository: React.FC = () => {
-  return <h1>Olá</h1>;
+  const { params } = useRouteMatch<RepositoryParams>();
+  return (
+    <h1>
+      Repositório:
+      {params.repository}
+    </h1>
+  );
 };
 
 export default Repository;
