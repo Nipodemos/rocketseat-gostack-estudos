df15e9fd766fbee9cf58124f17e1638ae8520301
Nipodemos <nipodemos13@gmail.com>
2020-09-08 21:44:01 -0300

    Fase 03 - Criando a aplicação: Estilizando detalhe

--- nivel03/primeiro-projeto-react/src/pages/Repository/index.tsx
+++ nivel03/primeiro-projeto-react/src/pages/Repository/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import logoImage from '../../assets/logo.svg';
+import { Header, RepositoryInfo, Issues } from './styles';
 
 interface RepositoryParams {
   repository: string;
@@ -8,10 +11,51 @@ interface RepositoryParams {
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
   return (
-    <h1>
-      Repositório:
-      {params.repository}
-    </h1>
+    <>
+      <Header>
+        <img src={logoImage} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars1.githubusercontent.com/u/11494727?v=4"
+            alt=""
+          />
+          <div>
+            <strong>rocketseaat/unform</strong>
+            <p>descrição do repositório</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>stars</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>forks</span>
+          </li>
+          <li>
+            <strong>64</strong>
+            <span>issues abertos</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="asdas">
+          <div>
+            <strong>issue</strong>
+            <p>repository.description</p>
+          </div>
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
   );
 };
 
