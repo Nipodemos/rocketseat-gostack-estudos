66095cc26381f38470bcd2206227c5b6577dd935
Nipodemos <nipodemos13@gmail.com>
2021-01-16 07:36:56 -0300

    Fase 03 - Iniciando o front end - Utilizando Unform

--- nivel03/gobarber_web/src/pages/SignUp/index.tsx
+++ nivel03/gobarber_web/src/pages/SignUp/index.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
+import { Form } from '@unform/web';
 import { Container, Content, Background } from './styles';
 import logoImg from '../../assets/logo.svg';
 import Input from '../../components/input';
 import Button from '../../components/button';
 
 const SignUp: React.FC = () => {
+  const handleSubmit = (data: Object): void => {
+    console.log(data);
+  };
+
   return (
     <Container>
       <Background />
       <Content>
         <img src={logoImg} alt="Logo do site" />
-        <form>
+        <Form onSubmit={handleSubmit}>
           <h1>Faça seu cadastro</h1>
           <Input icon={FiUser} name="name" type="text" placeholder="Nome" />
           <Input icon={FiMail} name="email" type="text" placeholder="Email" />
@@ -22,7 +27,7 @@ const SignUp: React.FC = () => {
             placeholder="Senha"
           />
           <Button type="submit">Cadastrar</Button>
-        </form>
+        </Form>
         <a href="create">
           <FiArrowLeft />
           Voltar para logon
