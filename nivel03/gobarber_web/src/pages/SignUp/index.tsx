fa22bc65603b34a13f8983d8b99771fa78648441
Nipodemos <nipodemos13@gmail.com>
2021-01-16 07:18:47 -0300

    Fase 03 - Iniciando Front end web - Página de cadastro

---
+++ nivel03/gobarber_web/src/pages/SignUp/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
+import { Container, Content, Background } from './styles';
+import logoImg from '../../assets/logo.svg';
+import Input from '../../components/input';
+import Button from '../../components/button';
+
+const SignUp: React.FC = () => {
+  return (
+    <Container>
+      <Background />
+      <Content>
+        <img src={logoImg} alt="Logo do site" />
+        <form>
+          <h1>Faça seu cadastro</h1>
+          <Input icon={FiUser} name="name" type="text" placeholder="Nome" />
+          <Input icon={FiMail} name="email" type="text" placeholder="Email" />
+          <Input
+            icon={FiLock}
+            name="password"
+            type="password"
+            placeholder="Senha"
+          />
+          <Button type="submit">Cadastrar</Button>
+        </form>
+        <a href="create">
+          <FiArrowLeft />
+          Voltar para logon
+        </a>
+      </Content>
+    </Container>
+  );
+};
+
+export default SignUp;
