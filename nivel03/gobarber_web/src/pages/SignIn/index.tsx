12577d0f13b5d2d1a052cb055a2539a8d598d5ac
Nipodemos <nipodemos13@gmail.com>
2021-01-27 02:24:29 -0300

    Fase 03 - Iniciando o front end web - Validação de login

--- nivel03/gobarber_web/src/pages/SignIn/index.tsx
+++ nivel03/gobarber_web/src/pages/SignIn/index.tsx
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
+import * as Yup from 'yup';
+import type { FormHandles } from '@unform/core';
+import getValidationErrors from '../../utils/getValidationErrors';
 import { Container, Content, Background } from './styles';
 import logoImg from '../../assets/logo.svg';
 import Input from '../../components/input';
 import Button from '../../components/button';
 
 const SignIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+  const handleSubmit = useCallback(async (data: Object) => {
+    try {
+      formRef.current?.setErrors({});
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .required('Email obrigatório')
+          .email('Digite um email válido'),
+        password: Yup.string().min(6, 'No mínimo 6 dígitos'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (error) {
+      const formattedErrors = getValidationErrors(error);
+      formRef.current?.setErrors(formattedErrors);
+      console.group('erro de validação do yup');
+      console.log('data :>> ', data);
+      console.error('error :>> ', error);
+      console.groupEnd();
+    }
+  }, []);
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="Logo do site" />
-        <form>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Faça seu login</h1>
           <Input icon={FiMail} name="email" type="text" placeholder="Email" />
           <Input
@@ -21,7 +48,7 @@ const SignIn: React.FC = () => {
           />
           <Button type="submit">Entrar</Button>
           <a href="forgot">Esqueci minha senha</a>
-        </form>
+        </Form>
         <a href="create">
           <FiLogIn />
           Criar conta
